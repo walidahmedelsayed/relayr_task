b186aaf47363327a7294f1e7d2ced651e4890a2d
Walid Ahmed <walidahmedelsayed1@gmail.com>
2022-03-22 23:53:20 +0200

    Add unit testing

---
+++ client/src/components/MeasurementsChart/MeasurementsChart.test.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { screen, render, fireEvent } from "@testing-library/react";
+import MeasurementsChart from "./MeasurementsChart";
+import devices from "../../fixtures/devices.json";
+
+describe("Test the chart component", () => {
+  it("render the chart component", () => {
+    render(<MeasurementsChart device={devices[0]} />);
+    const measurement = screen.getAllByText(/Measurement Name/i);
+    expect(measurement.length).toBe(2);
+  });
+});
